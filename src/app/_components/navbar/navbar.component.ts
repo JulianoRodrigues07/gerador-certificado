2e3297cf545667bb8580f3e8ab0cc4c06f48e2ad
JulianoRodrigues07 <94149364+JulianoRodrigues07@users.noreply.github.com>
2026-04-08 15:02:30 -0300

    preparando para o deploy

---
+++ src/app/_components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterModule, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent{
+}
